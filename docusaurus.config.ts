8dc19ad422749190571bff21b043dfb87f5cabf6
GreatWyrm <alecmusante@gmail.com>
2025-10-17 22:14:17 -0700

    Fix capitalization

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://nightfall-team.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/nightfall-website/',
+  baseUrl: '/Nightfall-Website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
